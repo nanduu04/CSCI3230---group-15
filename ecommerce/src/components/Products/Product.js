749e7cbfb7a62083ca2b76e6ca70d4f62253a504
sailesh10 <46660368+sailesh10@users.noreply.github.com>
2022-04-05 21:10:54 -0400

    display products

--- ecommerce/src/components/Products/Product.js
+++ ecommerce/src/components/Products/Product.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './App.css';
-function Product() {
-   return (
-      <div className="App">
-         Display Product
-      </div>
-   );
+import {
+    Box,
+    Heading,
+    Flex,
+    Link
+} from '@chakra-ui/react';
+import ProductCard from './Product/ProductCard'
+import Filters from './FilterDrawer'
+import Values from './Values';
+import { Link as lee } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+
+export default function Products() {
+    const { products } = useSelector(state => state.products)
+    return (
+        <Box w="100%" color="gray.600" >
+            <Box p={6} textAlign={'center'} border={'none'} >
+                <Flex justifyContent={'center'} flexDirection={'row'}>
+                    <Heading >Display All Products...</Heading>
+                    <Filters />
+                </Flex>
+            </Box>
+            <Flex justifyContent={'center'} flexWrap={'wrap'} >
+                {products.map(product => <Link to={`/shop/${product.id}`} as={lee} ><ProductCard data={product} /></Link>)}
+            </Flex>
+        </Box >
+    );
 }
-export default Product;
