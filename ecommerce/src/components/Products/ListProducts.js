55fb26a4945e2b2aca173f821cffc6fc14ddebe0
sailesh10 <46660368+sailesh10@users.noreply.github.com>
2022-04-05 21:07:15 -0400

    add product/list

---
+++ ecommerce/src/components/Products/ListProducts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+function ListProducts() {
+   return (
+      <div className="App">
+         List of all the products
+      </div>
+   );
+}
+export default ListProducts;
---
+++ ecommerce/src/components/Products/Product.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+function Product() {
+   return (
+      <div className="App">
+         Display Product
+      </div>
+   );
+}
+export default Product;
